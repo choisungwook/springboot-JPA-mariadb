5406512e81a26a48a31727a689819f30600e00c5
choisungwook <kgg1959@naver.com>
2021-08-01 18:28:07 +0900

    Add: 테스트용 Entity 추가 #4

---
+++ src/main/java/com/example/JPA/DemoEntity.java
@@ -0,0 +1,16 @@
+package com.example.JPA;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "abcde")
+public class DemoEntity {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private String name;
+}
